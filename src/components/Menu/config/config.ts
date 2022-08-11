49afc505d8c45d64615afc4bac521d3ffd0eabd1
Protocol BEAR <108098483+CaptainSend@users.noreply.github.com>
2022-08-11 15:09:33 +0200

    Update config.ts

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -12,14 +12,7 @@ import {
   MoreIcon,
 } from '@pancakeswap/uikit'
 
-import { DropdownMenuItems } from '@pancakeswap/uikit/src/components/DropdownMenu/types'
-import { ChainId } from '@pancakeswap/sdk'
-
-export type ConfigMenuDropDownItemsType = DropdownMenuItems & { hideSubNav?: boolean }
-export type ConfigMenuItemsType = Omit<MenuItemsType, 'items'> & { hideSubNav?: boolean } & {
-  items?: ConfigMenuDropDownItemsType[]
-}
-
+import { MenuItemsType, DropdownMenuItemType } from '@pancakeswap/uikit'
 import { ContextApi } from 'contexts/Localization/types'
 import { nftsBaseUrl } from 'views/Nft/market/constants'
 
@@ -81,29 +74,30 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
     ],
   },
   {
-    label: '',
-    href: '/info',
-    icon: MoreIcon,
-    hideSubNav: true,
-    supportChainIds: [ChainId.BSC],
-    items: [
-      {
-        label: t('Website'),
-        href: 'https://protocolBEAR.com',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-      {
-        label: t('Docs - coming soon'),
-        href: '/',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-      {
-        label: t('Community'),
-        href: 'https://t.me/ProtocolBEAR',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-    ],
-  },
+      label: '',
+      href: '/info',
+      icon: MoreIcon,
+      hideSubNav: true,
+      supportChainIds: [ChainId.BSC],
+      items: [
+        {
+          label: t('Website'),
+          href: 'https://protocolBEAR.com',
+          type: DropdownMenuItemType.EXTERNAL_LINK,
+        },
+        {
+          label: t('Docs - coming soon'),
+          href: '/',
+          type: DropdownMenuItemType.EXTERNAL_LINK,
+          
+        },
+        {
+          label: t('Community'),
+          href: 'https://t.me/ProtocolBEAR',
+          type: DropdownMenuItemType.EXTERNAL_LINK,
+        },
+      ],
+    },
 ]
 
 export default config
