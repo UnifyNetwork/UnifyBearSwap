21fcd34030bab9e8baf034ab463a793d556ad349
Protocol BEAR <108098483+CaptainSend@users.noreply.github.com>
2022-08-11 14:04:05 +0200

    Update config.ts
    Cleaned up after adding new menu features

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -60,30 +60,29 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
     ],
   },
   {
-      label: '',
-      href: '/info',
-      icon: MoreIcon,
-      hideSubNav: true,
-      supportChainIds: [ChainId.BSC],
-      items: [
-        {
-          label: t('Website'),
-          href: 'https://protocolBEAR.com',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-        },
-        {
-          label: t('Docs - coming soon'),
-          href: '/',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-          
-        },
-        {
-          label: t('Community'),
-          href: 'https://t.me/ProtocolBEAR',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-        },
-      ],
-    },
+    label: '',
+    href: '/info',
+    icon: MoreIcon,
+    hideSubNav: true,
+    supportChainIds: [ChainId.BSC],
+    items: [
+      {
+        label: t('Website'),
+        href: 'https://protocolBEAR.com',
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      },
+      {
+        label: t('Docs - coming soon'),
+        href: '/',
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      },
+      {
+        label: t('Community'),
+        href: 'https://t.me/ProtocolBEAR',
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      },
+    ],
+  },
 ]
 
 export default config
