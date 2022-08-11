2e6bf46b398ac1578f64f1639cbcb6e692186f43
Protocol BEAR <108098483+CaptainSend@users.noreply.github.com>
2022-08-11 21:03:34 +0200

    Update config.ts

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -6,22 +6,18 @@ export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
 
 const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   {
-    label: t('Exchange'),
+    label: t('Trade'),
     icon: 'Swap',
     href: '/swap',
     showItemsOnMobile: false,
     items: [
       {
-        label: t('Swap'),
+        label: t('Exchange'),
         href: '/swap',
       },
       {
         label: t('Liquidity'),
         href: '/liquidity',
-      },
-      {
-        label: t('Bridge'),
-        href: 'https://Bridge.BearSwap.net',
       },
     ],
   },
@@ -42,26 +38,17 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   },
   {
     label: 'More',
-    href: 'https://ProtocolBEAR.com',
+    href: '#',
     icon: 'More',
     hideSubNav: false,
     items: [
       {
-        label: t('Website'),
+        label: t('Bear Coin'),
         href: 'https://ProtocolBEAR.com',
         type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-      {
-          label: t('Docs - coming soon'),
-          href: '/',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-          
-        },
-        {
-          label: t('Community'),
-          href: 'https://t.me/ProtocolBEAR',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-        },
+      }
     ],
   },
 ]
+
+export default config
