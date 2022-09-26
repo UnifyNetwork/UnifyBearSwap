e7032becc451733b230b5717016861ea07a6feee
UnifyNetwork <114197986+UnifyNetwork@users.noreply.github.com>
2022-09-26 16:31:24 +0200

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -18,20 +18,6 @@ const pools: SerializedPoolConfig[] = [
     sortOrder: 1,
     isFinished: false,
   },
-  {
-    sousId: 1,
-    stakingToken: serializedTokens.bear,
-    earningToken: serializedTokens.cake,
-    contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0x1ab0d7291AF9Bf20A3f8c21c3A3d11682e5972C5',
-    },
-    poolCategory: PoolCategory.COMMUNITY,
-    harvest: true,
-    tokenPerBlock: '0.21',
-    sortOrder: 1,
-    isFinished: false,
-  }
   
 ]
 
